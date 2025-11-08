27d29a78a5dc3d592f8ccb9e660358fc407bcda3
Juan Alberto <zerocol101@gmail.com>
2025-11-09 00:50:05 +0100

    pushing changes

---
+++ C00/ex08/ft_sort_int_tab.c
@@ -0,0 +1,66 @@
+#include <unistd.h>
+
+/*void  ft_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+    if (nb < 0)
+    {
+        ft_putchar('-');
+        nb = -nb;
+    }
+    if (nb >= 10)
+        ft_putnbr(nb / 10);
+    ft_putchar((nb % 10) + '0');
+}
+*/
+void  ft_sort_int_tab(int *tab, int size)
+{
+  int i;
+  int j;
+  int temp;
+
+  i = 0;
+  while (i < size)
+  {
+    j = i + 1;
+    while (j < size)
+    {
+      if (tab[j] < tab[i])
+      {
+        temp = tab[i];
+        tab[i] = tab[j];
+        tab[j] = temp;
+      }
+      j++;
+    }
+    i++;
+  }
+}
+/*
+int main(void)
+{
+  int arr[] = {1, 25, 8, 238, 6};
+  int size = 5;
+  int i;
+
+  ft_sort_int_tab(arr, size);
+
+  i = 0;
+  while (i < size)
+  {
+    ft_putnbr(arr[i]);
+    if (i != size - 1)
+    {
+      ft_putchar(',');
+      ft_putchar(' ');
+    }
+    i++;
+  }
+  ft_putchar('\n');
+
+  return (0);
+}*/
