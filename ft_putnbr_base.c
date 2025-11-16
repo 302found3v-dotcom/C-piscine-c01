269c86a0a2373490a55791ebfc796f2f367c27a5
Juan Alberto <zerocol101@gmail.com>
2025-11-16 04:03:36 +0100

    ft_putnbr_base

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -9,19 +9,20 @@
 /*   Updated: 2025/11/16 02:56:29 by jreyes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
 void    ft_putchar(char c)
 {
     write(1, &c, 1);
 }
 
-void    check_ft_putnbr_base(char *base)
+int    check_ft_putnbr_base(char *base)
 {
     int j;
     int i;
 
     i = 0;
-    while (base[i])
+    while (base[i] != '\0')
     {
         if (base[i] == '-' || base[i] == '+')
             return (0);
@@ -41,16 +42,34 @@ void    check_ft_putnbr_base(char *base)
 
 void    ft_putnbr_base(int nbr, char *base)
 {
-    
-}
+    int len;
 
+    len = 0;
+    if (check_ft_putnbr_base(base))
+    {
+        while (base[len])
+            len++;
+        if (nbr < 0)
+        {
+            ft_putchar('-');
+            nbr = -nbr;
+        }
+        if (nbr >= len)
+            ft_putnbr_base(nbr / len, base);
+        ft_putchar(base[nbr % len]);
+    }
+    return;
+}
+/*
 #include <stdio.h>
+#include <stdlib.h>
 
 int	main(int argc, char **argv)
 {
 	if (argc > 1)
 	{
-		;
+        int nbr = atoi(argv[1]);
+	    ft_putnbr_base(nbr, argv[2]);
 	}
 	return (0);
-}
+}*/
